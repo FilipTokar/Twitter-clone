f2b76d09ae5b29c283286f6b3e246216b9b55088
Filip <filip.tokar@seznam.cz>
2023-04-29 16:54:54 +0200

    adding a twitterEmbed library to features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
 
 export default function Features() {
   return (
     <div className='features'>
-      <h1>Features</h1>
+      <div className="features__input">
+        <SearchIcon className="features__searchIcon"/>
+        <input type="text" placeholder='Search Twitter' />
+      </div>
+      <div className="features__widget-container">
+            <TwitterTweetEmbed tweetId='1649855285777899522'/>
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="elonmusk"
+                options={{height: 800}}
+                />
+      </div>
     </div>
   )
 }
